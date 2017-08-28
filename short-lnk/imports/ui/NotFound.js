c4d0788f807921ef2a14ee891bb7c5b611a311b1
Jason Cherry <jcherry@gmail.com>
2017-08-27 21:44:16 -0600

    updated 404

--- short-lnk/imports/ui/NotFound.js
+++ short-lnk/imports/ui/NotFound.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router';
 export default () => {
 	return (
 		<div className='boxed-view'><div className='box'>
-			<h1>Page Not Found</h1>
+			<h1>404 - Page Not Found</h1>
 			<p>Hmmm, we're unable to find that page.</p>
 			<Link to='/' className='button link'>Head Home</Link>
 		</div></div>
