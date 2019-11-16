448c8a447a3ed3f00e5375e24d430b913cdc4bf9
Jason M. Cherry <jcherry@gmail.com>
2019-11-15 21:08:55 -0800

    Tutorial: Video 14

--- score-keep/client/main.jsx
+++ score-keep/client/main.jsx
@@ -2,7 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
 
-const players = [];
+const players = [{
+	_id: '1',
+	name: 'Lauren',
+	score: 102,
+},{
+	_id: '3',
+	name: 'Andrew',
+	score: -12
+}];
+
+const renderPlayers = function(playersList) {
+	return playersList.map(function(player) {
+		return <p key={player._id}>{player.name} has {player.score} point(s).</p>;
+	});
+};
 
 Meteor.startup(function() {
 	var title = 'Account Settings';
@@ -11,6 +25,7 @@ Meteor.startup(function() {
 		<h1>{title}</h1>
 		<p>Hello {name}!</p>
 		<p>This is my second p.</p>
+		{renderPlayers(players)}
 	</div>);
 	ReactDOM.render(jsx, document.getElementById('app'));
 });
