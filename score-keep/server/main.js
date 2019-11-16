5ed9c2028b1e5fb9e1c19ba66734f8095f195a8c
Jason M. Cherry <jcherry@gmail.com>
2019-11-16 08:57:41 -0800

    Tutorial: Video 27

--- score-keep/server/main.js
+++ score-keep/server/main.js
@@ -10,17 +10,50 @@ Meteor.startup(() => {
 		}
 
 		getGreeting() {
-			return `Hi! I am ${this.name}`;
+			return `Hi! I am ${this.name}.`;
 		}
 
 		getPersonDescription() {
-			return `${this.name} is ${this.age} year(s) old.`
+			return `${this.name} is ${this.age} year(s) old.`;
 		}
 	}
 
-	var me = new Person('Jason');
+	class Employee extends Person {
+		constructor(name, age, title) {
+			super(name, age);
+			this.title = title;
+		}
+
+		getGreeting() {
+			if(this.title) {
+				return `Hi! I am ${this.name}. I work as a ${this.title}.`;
+			} else {
+				return super.getGreeting();
+			}
+		}
+
+		hasJob() {
+			return !!this.title;
+		}
+	}
+
+	class Programmer extends Person {
+		constructor(name, age, preferredLanguage='assembly') {
+			super(name, age);
+			this.preferredLanguage = preferredLanguage;
+		}
+
+		getGreeting() {
+			return `Hi! I am ${this.name}. I am a ${this.preferredLanguage} developer.`;
+		}
+	}
+
+	var me = new Employee('Jason', 36, 'db admin');
 	console.log(me.getGreeting());
 
-	var person = new Person('Erin', 36)
-	console.log(person.getPersonDescription())
+	var person = new Employee('Erin', 36);
+	console.log(person.getGreeting());
+
+	var programmer = new Programmer('Cody', 42, 'Python');
+	console.log(programmer.getGreeting())
 });
