d3353397bb090a15909763b49afbcdae4a116e9d
Jason M. Cherry <jcherry@gmail.com>
2019-11-17 08:36:35 -0800

    Tutorial: Video 39

--- score-keep/imports/ui/TitleBar.jsx
+++ score-keep/imports/ui/TitleBar.jsx
@@ -11,10 +11,10 @@ export default class TitleBar extends React.Component {
 	}
 
 	render() {
-		return (<div>
+		return (<div className="title-bar"><div className="wrapper">
 			<h1>{this.props.title}</h1>
 			{this.renderSubtitle()}
-		</div>);
+		</div></div>);
 	}
 }
 
