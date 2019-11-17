f55c73a261cc77348bbb3528334df64efddc947f
Jason M. Cherry <jcherry@gmail.com>
2019-11-17 06:55:04 -0800

    Tutorial: Video 33

--- score-keep/client/main.jsx
+++ score-keep/client/main.jsx
@@ -7,19 +7,13 @@ import ReactDOM from 'react-dom';
 import {Players} from './../imports/api/players.js';
 import TitleBar from './../imports/ui/TitleBar.jsx';
 import AddPlayer from './../imports/ui/AddPlayer.jsx';
-import Player from './../imports/ui/Player.jsx';
-
-const renderPlayers = (playersList) => {
-	return playersList.map((player) => {
-		return <Player key={player._id} player={player} />
-	});
-};
+import PlayerList from './../imports/ui/PlayerList.jsx';
 
 Meteor.startup(() => {
 	Tracker.autorun(() => {
 		var jsx = (<div>
 			<TitleBar title={'Score Keep'} subtitle={'By Jason M. Cherry'} />
-			{renderPlayers(Players.find().fetch())}
+			<PlayerList players={Players.find().fetch()} />
 			<AddPlayer />
 		</div>);
 		ReactDOM.render(jsx, document.getElementById('app'));
