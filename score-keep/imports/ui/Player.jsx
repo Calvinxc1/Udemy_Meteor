591133c8d15b0a2fb3cd12e599a915ccd6541768
Jason M. Cherry <jcherry@gmail.com>
2019-11-17 09:44:21 -0800

    Tutorial: Video 42

--- score-keep/imports/ui/Player.jsx
+++ score-keep/imports/ui/Player.jsx
@@ -7,13 +7,13 @@ export default class Player extends React.Component {
 	render() {
 		return (<div className="item">
 			<p>{this.props.player.name} has {this.props.player.score} point(s).</p>
-			<button onClick={() => {
+			<button className="button button--round" onClick={() => {
 				Players.update({'_id': this.props.player._id}, {'$inc': {'score': -1}});
 			}}>-1</button>
-			<button onClick={() => {
+			<button className="button button--round" onClick={() => {
 				Players.update({'_id': this.props.player._id}, {'$inc': {'score': 1}});
 			}}>+1</button>
-			<button onClick={() => {
+			<button className="button button--round" onClick={() => {
 				Players.remove({'_id': this.props.player._id});
 			}}>X</button>
 		</div>);
--- score-keep/imports/ui/PlayerList.jsx
+++ score-keep/imports/ui/PlayerList.jsx
@@ -6,7 +6,7 @@ export default class PlayerList extends React.Component {
 	renderPlayers() {
 		if(this.props.players.length === 0) {
 			return (<div className="item">
-				<p>Add your first player to get started!</p>
+				<p className="item__message">Add your first player to get started!</p>
 			</div>);
 		} else {
 			return this.props.players.map((player) => {
