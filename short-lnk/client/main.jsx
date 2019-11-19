d8d911a7d5777ab49d2237ed50899df6fc2f6a0c
Jason M. Cherry <jcherry@gmail.com>
2019-11-18 20:51:03 -0800

    Tutorial: 6.4 Video

--- short-lnk/client/main.jsx
+++ short-lnk/client/main.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import {Meteor} from 'meteor/meteor';
 
+import Login from './../imports/ui/Login.jsx';
 import Signup from './../imports/ui/Signup.jsx';
 import Link from './../imports/ui/Link.jsx';
+import NotFound from './../imports/ui/NotFound.jsx';
+
+const routes = (<BrowserRouter><Switch>
+	<Route exact path="/" component={Login} />
+	<Route exact path="/signup" component={Signup} />
+	<Route exact path="/links" component={Link} />
+	<Route component={NotFound} />
+</Switch></BrowserRouter>);
 
 Meteor.startup(() => {
-	ReactDOM.render(<Link />, document.getElementById('app'));;
+	ReactDOM.render(routes, document.getElementById('app'));;
 });
