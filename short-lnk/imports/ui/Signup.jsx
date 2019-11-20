8ae5a185af0c7551c41cd1e922dc151d17d5f02e
Jason M. Cherry <jcherry@gmail.com>
2019-11-19 17:21:41 -0800

    Tutorial: 6.6 Video

--- short-lnk/imports/ui/Signup.jsx
+++ short-lnk/imports/ui/Signup.jsx
@@ -2,10 +2,37 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 export default class Signup extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			error: ''
+		};
+
+		this.binder();
+	}
+
+	binder() {
+		this.onSubmit = this.onSubmit.bind(this);
+	}
+
+	onSubmit(event) {
+		event.preventDefault();
+
+		this.setState({error: 'Error test'});
+	}
+
 	render() {
 		return (<div>
 			<h1>Join Short Link</h1>
-			
+
+			{this.state.error ? <p>{this.state.error}</p> : undefined}
+
+			<form onSubmit={this.onSubmit}>
+				<input type="email" name="email" placeholder="Email"/>
+				<input type="password" name="password" placeholder="Password" />
+				<button>Create Account</button>
+			</form>
+
 			<Link to="/">Already have an account?</Link>
 		</div>);
 	}
